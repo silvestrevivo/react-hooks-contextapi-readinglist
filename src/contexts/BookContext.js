4b59302f9d3d1f0b970406c62ad9d6980534737e
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-07 22:38:42 +0200

    adding localStorage with useEffect hooks

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,10 +1,20 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  // if books variable changes, useEffect trigers a function
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    // with a function as third argument, we check here if there is a saved
+    // value on localStorage for books variable. Otherwise returns empty array
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
+  // useEffect hook saves data in localStorage each time that books changes
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books])
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
